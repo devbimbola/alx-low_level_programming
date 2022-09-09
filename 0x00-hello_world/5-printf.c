57bd76b1151aa327d587920435c87039dd2e6b1f
abimbola habib <abimbolahabib37@gmail.com>
2022-09-09 16:23:24 -0700

    barca

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line.
  *
  * Return : Always 0 (Success)
  */
